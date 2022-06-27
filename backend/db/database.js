a8940c871ec46a303ee89fbc6f581570f3e92e88
unknown <gadaboma@gmail.com>
2022-06-27 10:31:20 -0800

    database connection established

---
+++ backend/db/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+   const response = await mongoose.connect(process.env.MONGOOSE_URI)
+   console.log('MongoDB Connected to: '+response.connection.host)
+}
+
+module.exports = {
+    connectDB
+}
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const cors = require('cors')
+const { connectDB } = require('./db/database')
 
 dotenv.config()
 app.use(cors())
+connectDB()
 
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
-
     console.log('Server is up and listening on port: ' + port)
 
 })
